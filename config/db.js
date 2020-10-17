f3e17442ee5761ea7cf3fb9dd925dfb416aefd81
avijitmondal-navsoft <avijit.mondal@navsoft.in>
2020-10-17 13:48:48 +0530

    v2

---
+++ config/db.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+require('dotenv').config();
+
+var dbURL = process.env.mongoURL;
+
+// mongoose.connect(dbURL,{ useNewUrlParser: true });
+ 
+// var db = mongoose.connection;
+ 
+// db.on('error', console.error.bind(console, 'connection error:'));
+ 
+// db.once('open', function() {
+//   console.log("Connection Successful!");
+// });
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURL,{ 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            //useUnifiedTopology: true
+        });
+        console.log('mongo connected...');
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
