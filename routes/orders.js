d1c2297c279e29053a800de5d42ddcc94f73f239
avijitmondal-navsoft <avijit.mondal@navsoft.in>
2020-10-18 09:48:38 +0530

    oder

--- routes/orders.js
+++ routes/orders.js
@@ -4,10 +4,13 @@ const OrderController = require('../controllers/orderController');
 // const validate = require('express-validation');
 // const config = require('../config/index');
 // const paramValidation = require('../config/param-validation');
+const validator = require("../helpers/validator"); 
+const auth = require('../middleware/auth');
 
 router.get('/', OrderController.list);
 // router.get('/email', OrderController.sendEmail);
-router.post('/create', OrderController.placeOrder);
+// router.post('/create', OrderController.placeOrder);
+router.post('/create',auth,validator.makeOrder, OrderController.makeOrder);
 router.get('/:orderId', OrderController.get);
 router.param('orderId', OrderController.load);
 module.exports = router;
